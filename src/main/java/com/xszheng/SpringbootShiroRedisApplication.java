41b1a771fb244cff8f4ed52589912ff0a3866c96
xiaowoniu93 <1187637436@qq.com>
2018-04-03 23:41:32 +0800

    去除不规范代码，新增controller类

--- src/main/java/com/xszheng/SpringbootShiroRedisApplication.java
+++ src/main/java/com/xszheng/SpringbootShiroRedisApplication.java
@@ -2,19 +2,12 @@ package com.xszheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringbootShiroRedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootShiroRedisApplication.class, args);
 	}
 
-	@RequestMapping(value="/hello")
-	public String hello(){
-		return "hello";
-	}
 }
