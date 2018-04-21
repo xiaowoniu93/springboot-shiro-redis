aa5b9ba1f2e9da69ab29f98e9a5743dc5d0d93b7
zhengxiaosun <xiaosun.zheng@sv.remaintech.com>
2018-04-21 10:03:46 +0800

    aop问题

--- src/main/java/com/xszheng/aspect/ControllerValidatorAspect.java
+++ src/main/java/com/xszheng/aspect/ControllerValidatorAspect.java
@@ -1,16 +1,24 @@
 package com.xszheng.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.xszheng.util.JsonUtil;
 
-@Aspect
 @Component
+@Aspect
 public class ControllerValidatorAspect {
+	
+	@Pointcut("execution(* com.xszheng.controller.*.*(..)) && args(..,result)")
+	public void methodPoint(){}
 
 	@Around("execution(* com.xszheng.controller.*.*(..)) && args(..,result)")
 	public Object doAround(ProceedingJoinPoint pjp, BindingResult result) throws Throwable{
@@ -21,5 +29,15 @@ public class ControllerValidatorAspect {
 			return object;
 		}
 	}
+	
+//	@Before(value = "execution(* com.xszheng.controller.*.*(..)) && args(..,result)")
+//	public JSON beforeMethod(JoinPoint point, BindingResult  result){
+//		JSONObject json = new JSONObject();
+//		if(result.hasErrors()){
+//			return JsonUtil.newJson().addCode(400).addMessage(result.getFieldError().getDefaultMessage()).toJson();
+//		} else {
+//			return json;
+//		}
+//	}
 	
 }
